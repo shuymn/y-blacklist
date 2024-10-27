a5b41cc882e517d18b7f9a28f4fcb43eb921ab45
shuymn <mail@shuymn.me>
2024-10-27 18:19:28 +0900

    feat: initialize

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid gap-10">
+    <h1 class="text-4xl">{{ msg }}</h1>
+
+    <div class="px-6 grid gap-3">
+      <button type="button" @click="count++" class="btn btn-outline w-fit place-self-center">count is {{ count
+        }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
+    <div>
+      <p>
+        Install
+        <a class="link link-primary" href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+        in your IDE for a better DX
+      </p>
+      <p class="text-base-content/60">Click on the WXT and Vue logos to learn more</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<style scoped>
+a {
+  text-decoration: inherit;
+}
+</style>
